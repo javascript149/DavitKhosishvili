d972e9f95c5571fe7e58bb3ce5e00a1e530f610e
davit khosishvili <140189620+davitkhosishvili@users.noreply.github.com>
2023-09-10 18:09:33 +0400

    Add files via upload

---
+++ arreys.js
@@ -0,0 +1,5 @@
+let arrayOfColours =  ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+alert(arrayOfColours [0]);
+alert(arrayOfColours [6]);
+arrayOfColours.splice(3);
+alert(arrayOfColours)
---
+++ objects.js
@@ -0,0 +1,13 @@
+let person = {
+    firstName: "Mate",
+    lastName: "Gotua",
+    location:  "Tbilisi"
+}
+alert(person.firstName)
+person.birthYear = 1970; 
+alert(person.birthYear)
+delete person.location;
+alert(person.location);
+person.Age = 22;
+alert(person.Age)
+
